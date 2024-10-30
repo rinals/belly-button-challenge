1c63c62b9cb175f338ca0486b421218410ec306a
Rinal Shastri <rinal.shastri@outlook.com>
2024-10-30 16:36:32 -0700

    add buildMetadata function

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -1,22 +1,26 @@
 // Build the metadata panel
 function buildMetadata(sample) {
+  // Use `d3.json` to fetch the JSON data from the given URL
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-
+    const metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
-
+    const resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    const result = resultArray[0];
 
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    const PANEL = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
-
+    PANEL.html("");
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
-
+    Object.entries(result).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
   });
 }
 
